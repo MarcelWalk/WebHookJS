a59d4146c967bfca5c7aaefc6f6e163afd280237
Marcel Walk <walk.marcel.97@googlemail.com>
2020-09-25 20:12:06 +0200

    Initial Version

---
+++ index.js
@@ -0,0 +1,25 @@
+// Require express and body-parser
+var express = require('express');
+var path = require('path');
+const bodyParser = require("body-parser")
+var Git = require("nodegit");
+
+// Initialize express and define a port
+const app = express()
+const PORT = 9090
+
+// Tell express to use body-parser's JSON parsing
+app.use(bodyParser.json())
+app.post("/webhook", (req, res) => {
+    console.log(req.body.repository.clone_url) // Call your action on the request here
+    Git.Clone(req.body.repository.clone_url, "test").then(function (repository) {
+        // Work with the repository object here.
+    });
+    res.status(200).end() // Responding is important
+})
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// Start express on the defined port
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
