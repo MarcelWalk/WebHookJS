6e80922507bbac55f3a3976a4a726f7bfc5e9d06
Marcel Walk <walk.marcel.97@googlemail.com>
2020-09-26 03:36:35 +0200

    base for comparing secrets

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser")
 const express = require('express');
 const git = require("nodegit");
 const path = require('path');
+var crypto = require('crypto');
+const fs = require('fs-extra')
 var ip = require("ip");
 var config = require('./config.json');
 
@@ -13,9 +15,20 @@ const app = express()
 const PORT = config.port
 
 // Tell express to use body-parser's JSON parsing
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.post("/" + config.hook_path, (req, res) => {
-    console.log(req.body.repository.clone_url) // Call your action on the request here
+
+    console.log(req.rawHeaders)
+    console.log(req.rawHeaders[19])
+
+    hmac = crypto.createHmac('sha1', config.password)
+    hmac.update(req.body.toString())
+    var check = hmac.digest('hex')
+
+    console.log("sha1=" + check);
+    console.log(check === req.rawHeaders[19])
+    
+    fs.removeSync(config.clone_directory)
     git.Clone(req.body.repository.clone_url, config.clone_directory).then(function (repository) {
         // Work with the repository object here.
     });
