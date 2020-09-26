b7d8079b4b9cfe6a5e54c4ed84d16d5b925a7c40
Marcel Walk <walk.marcel.97@googlemail.com>
2020-09-26 02:29:20 +0200

    removed frontend added config

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 // Require express and body-parser
-const express = require('express');
-const path = require('path');
 const bodyParser = require("body-parser")
+const express = require('express');
 const git = require("nodegit");
-const settings = require('./js/settings')
+const path = require('path');
+
+var config = require('./config.json');
+
 
 // Initialize express and define a port
 const app = express()
-const PORT = 9090
+const PORT = config.port
 
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json())
-app.post("/webhook", (req, res) => {
+app.post("/" + config.hook_path, (req, res) => {
     console.log(req.body.repository.clone_url) // Call your action on the request here
-    git.Clone(req.body.repository.clone_url, "test").then(function (repository) {
+    git.Clone(req.body.repository.clone_url, config.clone_directory).then(function (repository) {
         // Work with the repository object here.
     });
     res.status(200).end() // Responding is important
 })
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/html/index.html'));
-});
-app.get('/settings', function (req, res) {
-    res.sendFile(path.join(__dirname + '/html/index.html'));
-});
 
 // Start express on the defined port
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`http://[YOUR IP]:${PORT}`+"/" + config.hook_path ))
